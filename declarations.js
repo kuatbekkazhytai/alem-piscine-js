6bac5905e809c46f010c14b878743ca0ef1e3802
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-08 21:04:12 +0600

    4

--- declarations.js
+++ declarations.js
@@ -1,17 +1,17 @@
 const escapeStr = "`,\\,/,\",'";
 const arr = [4, '2'];
 const obj = {
-    str = 'hello world',
-    num = 5,
-    bool = true,
-    undef = undefined,
+    'str' : 'hello world',
+    'num' : 5,
+    'bool' : true,
+    'undef' : undefined,
 };
 const nested = {
-    arr = [4, undefined, '2'],
-    obj = {
-        str = 'hello',
-        num = 5,
-        bool = false,
+    'arr' : [4, undefined, '2'],
+    'obj' : {
+        'str' : 'hello',
+        'num' : 5,
+        'bool' : false,
     },
 };
 Object.freeze(obj);
