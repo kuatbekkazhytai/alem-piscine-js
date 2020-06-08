9459140557c32ac87d7778cebd635e97a2eea528
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-09 00:07:33 +0600

    lastkiss

---
+++ last-first-kiss.js
@@ -0,0 +1,10 @@
+const first = function(strOrArr) {
+    return strOrArr[0];
+};
+const first = function(strOrArr) {
+    return strOrArr[strOrArr.length-1];
+};
+
+const kiss = function(strOrArr) {
+    returns [strOrArr[0], strOrArr[strOrArr.length-1]];
+};
