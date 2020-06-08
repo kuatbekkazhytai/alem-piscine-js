928e2d544b4b6e54565c1c8173e5c3a5d2b1aa0b
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-09 00:04:04 +0600

    returnssssss

--- returns.js
+++ returns.js
@@ -1,6 +1,14 @@
-var idiwka;
-const returns = (idiwka) => idiwka;
-const id = returns(idiwka);
-let str;
-const ret = (str) => str.length;
-const getLength = ret(str)
+// var idiwka;
+// const returns = (idiwka) => idiwka;
+// const id = returns(idiwka);
+// let str;
+// const ret = (str) => str.length;
+// const getLength = ret(str)
+
+const id = function(num) {
+    return num;
+};
+
+const getLength = function(strOrArray) {
+    return strOrArray.length;
+};
