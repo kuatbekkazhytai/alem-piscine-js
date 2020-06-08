8229af6a204005a5461f359adf6644b69e23150d
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-08 22:33:50 +0600

    mutability

--- mutability.js
+++ mutability.js
@@ -6,7 +6,11 @@
     const clone1 = Object.assign({}, person);
     const clone2 = Object.assign({}, person);
 
-    const samePerson = person;
+    const samePerson = {
+        name: 'Rick',
+            age: 77,
+            country: 'US',
+    };
 
     person.age = person.age + 1;
     person.country = 'FR';
