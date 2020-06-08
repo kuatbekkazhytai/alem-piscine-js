f990169631ba5c2388c7c51794664abe7fa65c9f
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-09 00:32:28 +0600

    change

--- change.js
+++ change.js
@@ -3,7 +3,7 @@
 const get = function(key) {
     return sourceObject['key'];
 };
-const get = function(key, value) {
+const set = function(key, value) {
     sourceObject['key'] = value;
     return sourceObject;
 };
