aa1040fba5acf4ac6aea821d56f8c2922fcb172b
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-09 14:53:55 +0600

    dogyears

---
+++ dog-years.js
@@ -0,0 +1,13 @@
+var object = {
+    earth : 1.0,
+    mercury  : 0.2408467,
+    venus  : 0.61519726,
+    mars  : 1.8808158,
+    jupiter  : 11.862615,
+    saturn  : 29.447498,
+    uranus  : 84.016846,
+    neptune : 164.79132
+};
+
+const dogYears = (name, age) => age * 7 / name / 31557600;
+console.log(dogYears(object.earth, 1000000000).toFixed([2]));
