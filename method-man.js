bd5960408f1bdb7b018d311001f356db42798643
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-10 01:15:46 +0600

    methods

---
+++ method-man.js
@@ -0,0 +1,6 @@
+const words = (str) => str.split(' ');
+const sentence = (arr) => arr.join(' ');
+const yell = (str) => str.toUpperCase();
+const whisper = (str) => '*' + str.toLowerCase() + '*';
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
+
