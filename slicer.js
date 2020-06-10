2d91183e853c8dc44cd227ac9ee3964cef5d85b9
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-11 00:22:05 +0600

    slicer

--- slicer.js
+++ slicer.js
@@ -9,18 +9,18 @@ const slice = (strOrArr, start, end) => {
     if (typeof strOrArr === 'string') {
        
         var res = ''
-        for (i = start; i < finish; i++) {
+        for (let i = start; i < finish; i++) {
             res+=strOrArr[i]
         }
         return res
     } else {
         var res = []
-        for (i = start; i < finish; i++) {
+        for (let i = start; i < finish; i++) {
             res.push(strOrArr[i])
         }
         return res
     }
 }
 
-// console.log(slice([1,2,3,4,5,6], 3, 5))
+console.log(slice('abcdef', 2))
 // console.log([1,2,3,4,5,6].slice(3,5))
