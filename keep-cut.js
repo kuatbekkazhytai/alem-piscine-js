81e48196a9d1f8ec29046a67bbc5e378c87fd395
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-11 01:09:37 +0600

    cut

--- keep-cut.js
+++ keep-cut.js
@@ -24,30 +24,40 @@ const cutFirstLast = (str) => {
     return output
 }
 const keepFirst = (str) => {
-    var res = ''
-    for (let i = 0; i < 2; i++) {
-        res += str[i]
+    if (str.length > 2 ) {
+        var res = ''
+        for (let i = 0; i < 2; i++) {
+            res += str[i]
+        }
+        return res
     }
-    return res
+    return str
 }
 const keepLast = (str) => {
-    var res = ''
-    for (let i = str.length-2; i < str.length; i++) {
-        res += str[i]
+    if (str.length > 2 ) {
+        var res = ''
+        for (let i = str.length-2; i < str.length; i++) {
+            res += str[i]
+        }
+        return res
     }
-    return res
+    return str
+
 }
 const keepFirstLast  = (str) => {
-    var res = ''
-    var second = ''
-    for (let i = 0; i < 2; i++) {
-        res += str[i]
+    if (str.length > 2 ) {
+        var res = ''
+        var second = ''
+        for (let i = 0; i < 2; i++) {
+            res += str[i]
+        }
+        // console.log(res)
+        for (let i = str.length-2; i < str.length; i++) {
+            second += str[i]
+        }
+        return res + second
     }
-    // console.log(res)
-    for (let i = str.length-2; i < str.length; i++) {
-        second += str[i]
-    }
-    return res + second
+    return str
 }
 
 
