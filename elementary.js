05f2269bf9d412a05556e7e72db57a4a38deafe8
Kuatbek <kuatbek.kazhytai@nu.edu.kz>
2020-06-10 20:49:21 +0600

    math

--- elementary.js
+++ elementary.js
@@ -1,20 +1,33 @@
 const multiply = (a, b) => {
+    var neg = true
+    if (((a>0) && (b>0)) || ((a<0) && (b<0))) {
+        neg = false
+    } else {
+        neg = true
+    }
+    a = Math.abs(a)
+    b = Math.abs(b)
+
     var res = 0;
     for (let i = 0; i < b; i ++ ) {
         res+=a
     }
+
+    if (neg) {
+        return res * -1
+    }
     return res
 };
-const divide = (a,b) =>{
-    var neg = false
+// const divide = (a,b) =>{
+//     var neg = false
 
-    resInt = 0
-    while (a >= b) {
-        a -= b
-        resInt++
-    } 
-    return resInt
-};
-const modulo = (a,b) => a%b;
+//     resInt = 0
+//     while (a >= b) {
+//         a -= b
+//         resInt++
+//     } 
+//     return resInt
+// };
+// const modulo = (a,b) => a%b;
 
-// console.log(divide(30,5))
+console.log(multiply(4,0))
